bb22bf4e6b53c300bc774bfcc7fffafb58877bd4
Karolina <karolina.grzeszkiewicz@u.yale-nus.edu.sg>
2022-01-12 15:36:42 +0100

    initial commit

---
+++ bin/main.ml
@@ -0,0 +1,143 @@
+open Unix
+open ChatUtil
+
+let get_my_inet_addr () =
+  let my_name = Unix.gethostname() 
+  in let my_entry_by_name = gethostbyname my_name
+  in my_entry_by_name.Unix.h_addr_list.(0);;
+
+let get_inet_addr socket_addr =
+  match socket_addr with
+  | Unix.ADDR_INET (addr, _) -> Unix.string_of_inet_addr addr
+  | _ -> failwith "not INET"
+
+let get_port socket_addr =
+  match socket_addr with
+  | Unix.ADDR_INET (_, port) -> port 
+  | _ -> failwith "not INET"
+
+
+(* default values *)
+let is_server = ref true
+let server_port = ref 1234
+let server_ip = ref "127.0.0.1"
+let server_address = get_my_inet_addr ()
+
+(* parse command line arguments *)
+
+let set_args () =
+  match Sys.argv.(1) with
+  | "client" -> 
+    is_server := false;
+    Printf.printf "launching client mode\n"
+  | "server" -> Printf.printf "launching server mode\n"
+  | _ -> Printf.printf "incorrect input : must be either server or client, launching server mode\n";
+  if Array.length Sys.argv > 2
+  then 
+    (server_ip :=  Sys.argv.(2);
+    Printf.printf "for server with IP address: %s \n" !server_ip);
+  if Array.length Sys.argv > 3
+  then 
+    (server_port := int_of_string Sys.argv.(3);
+    Printf.printf "and with port number: %d \n" !server_port)
+
+
+let parse_args () =
+  match Array.length Sys.argv with
+  | 0 | 1 -> 
+    Printf.printf "arguments missing: launching server mode and using default server port number: %d, and host address: %s \n \n" !server_port !server_ip
+  | 2 -> 
+    set_args();
+    Printf.printf "arguments missing: using default server port number: %d, and host address: %s \n \n" !server_port !server_ip
+  | 3 ->
+    set_args();
+    Printf.printf "arguments missing: using default server host address: %s \n \n" !server_ip
+  | _ -> set_args()
+
+  (* guarantees that in case client launches without specifying port number and ip address 
+    and the server is already launched, it will use the launched server's port and ip address by default *)
+
+  (* TODO: what if client launches before the server? *)
+
+(* SERVER *)
+
+let run_server server_descr server_addr = 
+  Unix.bind server_descr server_addr;
+  Unix.listen server_descr 1; (* waiting for 1 client only *)
+  print_endline "The server is waiting for client to connect";
+  while true do
+    let (client_descr, client_addr) = Unix.accept server_descr in
+    Printf.printf "Connected to client: %s: %d \n" (get_inet_addr client_addr) (get_port client_addr);
+    let pid = Unix.fork() (* create child process to handle requests *)
+    in match pid with
+    | 0 -> (* child code *)
+      let ic = Unix.in_channel_of_descr client_descr 
+      and oc = Unix.out_channel_of_descr client_descr in
+      receiver ic oc;
+      close_in ic;
+      close_out oc;
+      Printf.printf "Client left the chat\n";
+      exit 0
+    | id ->  (* parent code *)
+      Unix.close client_descr;
+      ignore(Unix.waitpid [] id)
+  done
+
+  (* TODO: 
+  after connection is terminated by the client server continues waiting for 
+  another client
+  *)
+
+  (* TODO: server_func
+    upon receiving a message msg:
+    1. acknowledge the message by sending back "message received"
+    2. show the roundtrip time for acknowledgement
+    3. reply 
+
+    e.g. the terminal output should be sth like:
+    client sent: hello
+    sent to client: Message Received
+    reply: hi
+    Roundtrip time: 0.04 s
+
+
+  *)
+
+(* CLIENT *)
+
+(* command-line arguments: IP address / hostname of server*)
+
+
+let run_client client_descr server_addr  =
+  Printf.printf "Trying to connect as client: %s: %d to server: %s: %d \n" 
+  (get_inet_addr (Unix.getsockname client_descr)) (get_port (Unix.getsockname client_descr)) 
+  (get_inet_addr server_addr) (get_port server_addr);
+  (*Unix.connect client_descr server_addr;*)
+  let ic, oc = open_connection server_addr in
+  print_endline "Connected!";
+  (* call client_func *)
+  sender ic oc
+
+
+
+  
+
+(* TEST PARSING *)
+
+(*let () =
+  if Array.length Sys.argv < 3
+  then Printf.printf "arguments missing"
+  else Printf.printf "first arg: %s" Sys.argv.(1)
+  in Printf.printf "second arg: %s" Sys.argv.(2);;*)
+
+  let () =
+    parse_args ();
+    let server_descr = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in 
+    let server_addr = Unix.ADDR_INET (Unix.inet_addr_of_string !server_ip, !server_port) in 
+    let client_descr = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0
+    in if (!is_server)
+    then run_server server_descr server_addr
+    else run_client client_descr server_addr
+
+
+
---
+++ lib/chatUtil.ml
@@ -0,0 +1,86 @@
+(* 
+let copy_channels ic oc =
+  try 
+    while true do 
+      let s = input_line ic 
+      in if s = "END" then raise End_of_file 
+      else (output_string oc (s^"\n"); flush oc)
+    done
+  with End_of_file -> ();;
+*)
+(* 
+receiver
+1. prints the received message
+2. sends "message received"  
+*)
+
+(*
+sender
+1. receives and prints "message received" 
+2. calculates and prints roundtrip time
+3. sends another message
+*)
+open UnixLabels
+
+let t = ref 0.
+
+let receiver ic oc =
+  try 
+    while true do 
+      let r = input_line ic in 
+      Printf.printf "Received: %s \n" r;
+      flush oc;
+      output_string oc ("message received");
+      print_endline "sent ack";
+      (*in Printf.printf "Received: %s \n" r;
+      if r = "END" then 
+        (shutdown_connection ic; 
+        (*output_string oc "EXIT";
+        flush oc;*)
+        raise Exit)
+      else 
+        (flush Stdlib.stdout;
+        flush oc;
+        output_string oc "message received"; (* should pass this to the sender of the received message *)
+        print_endline "sent acknowledgement!";
+        (*flush oc*));*)
+    done 
+  with 
+    | Exit -> exit 0 
+    | exn -> shutdown_connection ic; raise exn;;
+
+let sender ic oc = 
+  (*let rec wait () = 
+    try 
+      let r = input_line ic in (* does it wait for input line ? *)
+      print_endline r;
+      match r with 
+      | "message received" -> 
+        print_endline r; (* ? *)
+        flush Stdlib.stdout;
+        flush oc;
+        Printf.printf "Roundtrip time: %fs\n" (Unix.gettimeofday () -. !t)
+        (*output_string oc r;
+        flush oc*)
+      | "EXIT" -> shutdown_connection ic; raise Exit
+      | _ -> Printf.printf "sender got an unexpected message: %s" r
+    with 
+      | _ -> print_endline "eh"; wait ()
+  in*) try 
+    while true do 
+      (* send message *)
+      t := Unix.gettimeofday ();
+      print_string "Send: ";
+      flush Stdlib.stdout;
+      output_string oc ((input_line Stdlib.stdin)^"\n");
+      flush oc (* ? *);
+      print_endline "finished writing";
+      (* wait for acknowledgement *)
+      let r = input_line ic in
+      Printf.printf "Response: %s \n \n " r;
+      if r = "END" then (shutdown_connection ic; raise Exit)
+      (*wait ()*)
+    done 
+  with 
+  | Exit -> exit 0 
+  | exn -> shutdown_connection ic; raise exn
