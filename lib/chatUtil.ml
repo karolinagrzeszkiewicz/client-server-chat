90487b782a6cd19cbad5e388a86bce41142ed303
Karolina <karolina.grzeszkiewicz@u.yale-nus.edu.sg>
2022-01-25 23:27:15 +0100

    this seems to be working

--- lib/chatUtil.ml
+++ lib/chatUtil.ml
@@ -2,6 +2,20 @@ open UnixLabels
 
 let t = ref 0.
 
+let rec send_msg oc msg =
+  output_string oc (msg^"\n");
+  let new_msg = input_line Stdlib.stdin in
+  begin match new_msg with
+  | "" -> output_string oc (new_msg^"\n");
+  | _ -> send_msg oc new_msg
+  end
+
+let rec read_msg ic acc =
+  begin match input_line ic with
+  | "" -> acc 
+  | my_line -> read_msg ic (acc^my_line^"\n")
+  end
+
 
 let rec chat ic oc my_turn online = 
   begin match online with
@@ -9,34 +23,30 @@ let rec chat ic oc my_turn online =
     if my_turn
     then 
       (print_string "Send: ";
-      flush Stdlib.stdout;
-      output_string oc ((input_line Stdlib.stdin)^"\n");
+      flush Stdlib.stdout; 
+      output_string oc "msg: ";
+      let msg = input_line Stdlib.stdin in
+      send_msg oc msg;
       t := Unix.gettimeofday ();
       flush oc;
       chat ic oc false true)
     else 
       (let r = input_line ic 
-      in Printf.printf "Received: %s \n\n" r;
-      flush Stdlib.stdout;
-      if r = "Message Received"
-      then (Printf.printf "Roundtrip time: %fs\n" (Unix.gettimeofday () -. !t);
-      flush Stdlib.stdout;
-      chat ic oc false true)
-      else 
-      (let r = "Message Received"
-      in output_string oc (r^"\n"); 
-      flush oc;
-      chat ic oc true true))
-      (* 
-          Printf.printf "Roundtrip time: %fs\n" (Unix.gettimeofday () -. !t);
-          flush Stdlib.stdout;
-          chat ic oc false 
-      *)
-          (*if r = "end" 
-          then 
-          (Printf.printf "The other side left the chat. \n\n";
-          shutdown_connection ic; 
-          raise Exit);*)
+      in begin match String.sub r 0 5 with
+      | "msg: " -> 
+        let msg = if (String.length r > 5) then read_msg ic ((String.sub r 5 ((String.length r)-5))^"\n") else read_msg ic ""
+        in Printf.printf "Received: %s \n\n" msg;
+        flush Stdlib.stdout;
+        output_string oc ("ack: Message Received\n"); 
+        flush oc;
+        chat ic oc true true
+      | "ack: " -> 
+        print_endline "Message Received\n";
+        Printf.printf "Roundtrip time: %fs\n\n" (Unix.gettimeofday () -. !t);
+        flush Stdlib.stdout;
+        chat ic oc false true
+      | _ -> ()
+      end)
   | false -> ()
   end
 
